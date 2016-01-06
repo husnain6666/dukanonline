eb82512433c4af43aedd1c435f46f473280f6b2b
Khalid Mansoor <khalidmansoor42@yahoo.com>
2016-01-06 12:34:33 +0500

    changed sidebar

--- admin/sideBar.php
+++ admin/sideBar.php
@@ -17,52 +17,52 @@
         <ul class="sidebar-menu">
             <li class="header">MAIN NAVIGATION</li>
 
-            <li class="<?php if($_SERVER['REQUEST_URI'] == "/HONLIZ.COM/data.php")
+            <li class="<?php if($_SERVER['REQUEST_URI'] == "/dukanonline/admin/data.php")
                 echo "active ";?>treeview">
                 <a href="data.php">
                     <i class="fa fa-user"></i> <span>Customer</span> <i class=""></i>
                 </a>
             </li>
-            <li class="<?php if($_SERVER['REQUEST_URI'] == "/HONLIZ.COM/orders.php")
+            <li class="<?php if($_SERVER['REQUEST_URI'] == "/dukanonline/admin/orders.php")
                 echo "active ";?>treeview">
                 <a href="orders.php">
                     <i class="fa fa-book"></i> <span>Order</span> <i class=""></i>
                 </a>
             </li>
 
-            <li class="<?php if($_SERVER['REQUEST_URI'] == "/HONLIZ.COM/addArticle.php")
+            <li class="<?php if($_SERVER['REQUEST_URI'] == "/dukanonline/admin/addArticle.php")
                 echo "active ";?>treeview">
                 <a href="addArticle.php">
                     <i class="fa fa-plus"></i> <span>Add New Article</span> <i class=""></i>
                 </a>
             </li>
-            <li class="<?php if($_SERVER['REQUEST_URI'] == "/HONLIZ.COM/articles.php")
+            <li class="<?php if($_SERVER['REQUEST_URI'] == "/dukanonline/admin/articles.php")
                 echo "active ";?>treeview">
                 <a href="articles.php">
                     <i class="fa fa-star"></i> <span>Article</span> <i class=""></i>
                 </a>
             </li>
             </li>
-            <li class="<?php if($_SERVER['REQUEST_URI'] == "/HONLIZ.COM/addBrand.php")
+            <li class="<?php if($_SERVER['REQUEST_URI'] == "/dukanonline/admin/addBrand.php")
                             echo "active ";?> treeview">
                 <a href="addBrand.php">
                     <i class="fa fa-apple"></i> <span>Add Brand Name</span> <i class=""></i>
                 </a>
             </li>
             </li>
-            <li class="<?php if($_SERVER['REQUEST_URI'] == "/HONLIZ.COM/addCategory.php")
+            <li class="<?php if($_SERVER['REQUEST_URI'] == "/dukanonline/admin/addCategory.php")
                 echo "active ";?>treeview">
                 <a href="addCategory.php">
                     <i class="fa fa-plus"></i> <span>Add Category Name</span> <i class=""></i>
                 </a>
             </li>
-            <li class="<?php if($_SERVER['REQUEST_URI'] == "/HONLIZ.COM/phpMail.php")
+            <li class="<?php if($_SERVER['REQUEST_URI'] == "/dukanonline/admin/phpMail.php")
                 echo "active ";?>treeview">
                 <a href="phpMail.php">
                     <i class="fa fa-mail-forward"></i> <span>Send Newsletter</span> <i class=""></i>
                 </a>
             </li>
-            <li class="<?php if($_SERVER['REQUEST_URI'] == "/HONLIZ.COM/addPoster.php")
+            <li class="<?php if($_SERVER['REQUEST_URI'] == "/dukanonline/admin/addPoster.php")
                 echo "active ";?>treeview">
                 <a href="addPoster.php">
                     <i class="fa fa-plus"></i> <span>Add Poster</span> <i class=""></i>
